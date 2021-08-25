2f7b7b699b758fc14ec2264f008d8aed17691359
Ivan Grishin <ktezik@yandex.ru>
2021-08-25 23:55:59 +0300

    View setup

---
+++ loginMenuApp/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  loginMenuApp
+//
+//  Created by Иван Гришин on 25.08.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
