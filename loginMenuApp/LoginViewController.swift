495938c85d1b64f54959c17191c6c2b82e14cf60
Ivan Grishin <ktezik@yandex.ru>
2021-08-26 16:32:09 +0300

    Work with logic and second view controller

--- loginMenuApp/LoginViewController.swift
+++ loginMenuApp/LoginViewController.swift
@@ -18,19 +18,31 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         logInLabel.layer.cornerRadius = 10
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcome = segue.destination as? WelcomeViewController else { return }
+        welcome.userNameLabelData = usernameOutlet.text
+    }
     
     @IBAction func forgotUsernameAlert() {
         alertAction("Forgot Name", "Your username is: Username")
+        
     }
     @IBAction func forgotPasswordAlert() {
         alertAction("Forgot Password", "Your password is: Password")
     }
     @IBAction func logInAction() {
         if usernameOutlet.text == "Username" && passwordOutlet.text == "Password" {
-            alertAction("Nice", "You In")
+//            alertAction("Nice", "You In")
         } else {
             alertAction("Wrong Password or Username", "Click Forgot Password and Forgot Username")
+            passwordOutlet.text = ""
         }
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard segue.source is WelcomeViewController else { return }
+        usernameOutlet.text = ""
+        passwordOutlet.text = ""
     }
     
     private func alertAction(_ title: String, _ message: String) {
---
+++ loginMenuApp/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeViewController.swift
+//  loginMenuApp
+//
+//  Created by Иван Гришин on 26.08.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var userNameLabel: UILabel!
+    
+    @IBOutlet var logOutLabel: UIButton!
+    
+    var userNameLabelData: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logOutLabel.layer.cornerRadius = 10
+        
+        userNameLabel.text = userNameLabelData
+    }
+    
+}
