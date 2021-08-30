215df6295be03fde2cdf4f6b3529e5f923040ed7
Ivan Grishin <ktezik@yandex.ru>
2021-08-30 13:27:43 +0300

    addicting Models

---
+++ loginMenuApp/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  loginMenuApp
+//
+//  Created by Иван Гришин on 30.08.2021.
+//
+
+import Foundation
+
+
+struct User {
+    let name = "Name"
+    let password = "Password"
+    let surname = "Surname"
+    let moreInfo = "I was born in NewYork, me 22, i love footboal"
+}
